a13f2466cf1f46253fbbaadd01eced5a55093a43
geekxing <lxb332501>
2018-04-11 14:04:15 +0800

    add

--- Server/RouterPattern/public/javascripts/image.server.js
+++ Server/RouterPattern/public/javascripts/image.server.js
@@ -5,7 +5,6 @@ let imgSuffixes = ["png", "gif", "jpeg", "jpg"]
 
 http.createServer((req, res) => {
     console.log(req.headers.host + req.url);
-    console.log("current path is: " + __dirname);
 
     let path = '..' + decodeURI(req.url)
 
